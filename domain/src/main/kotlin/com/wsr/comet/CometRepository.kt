9fcec225b73d547f5eae4a05444819bfa87bb981
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2024-11-24 22:14:20 +0900

    [add] getOwnerCometsを追加

--- domain/src/main/kotlin/com/wsr/comet/CometRepository.kt
+++ domain/src/main/kotlin/com/wsr/comet/CometRepository.kt
@@ -1,6 +1,9 @@
 package com.wsr.comet
 
+import com.wsr.user.UserId
+
 interface CometRepository {
-    suspend fun getComets(limit: Int, offset: Int): List<Comet>
     suspend fun getComet(id: CometId): Comet
+    suspend fun getComets(limit: Int, offset: Int): List<Comet>
+    suspend fun getOwnedComets(ownerId: UserId.OwnerId, limit: Int, offset: Int): List<Comet>
 }
