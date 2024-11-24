b64ee7bcaf29b5d5771db11599af17e62db2c455
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2024-11-24 22:10:30 +0900

    [fix] tail -> dust

--- domain/src/main/kotlin/com/wsr/comet/Comet.kt
+++ domain/src/main/kotlin/com/wsr/comet/Comet.kt
@@ -1,13 +1,12 @@
 package com.wsr.comet
 
-import com.wsr.user.UserId
 import java.util.*
 
 data class Comet(
     val id: CometId = CometId(UUID.randomUUID().toString()),
     val core: Core,
     val coma: Coma,
-    val tails: Map<UserId.ObserverId, List<Tail>>,
+    val tails: List<Tail>,
 )
 
 @JvmInline
--- domain/src/main/kotlin/com/wsr/comet/Tail.kt
+++ domain/src/main/kotlin/com/wsr/comet/Tail.kt
@@ -1,15 +1,25 @@
 package com.wsr.comet
 
+import com.wsr.user.UserId
 import java.util.*
 
 data class Tail(
-    val id: TailId = TailId(UUID.randomUUID().toString()),
+    val tailId: TailId,
+    val observerId: UserId.ObserverId,
+    val dusts: List<Dust>,
+)
+
+data class Dust(
+    val id: DustId = DustId(UUID.randomUUID().toString()),
     val posterType: Position,
     val content: Content,
 )
 
 @JvmInline
 value class TailId(val value: String)
+
+@JvmInline
+value class DustId(val value: String)
 
 sealed interface Position {
     data object Owner : Position
