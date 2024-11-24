5945b1dd8881b56c4154d2c0201bb9250e0a9b35
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2024-11-24 20:29:43 +0900

    [add] UserRepositoryを追加

---
+++ domain/src/main/kotlin/com/wsr/user/UserRepository.kt
@@ -0,0 +1,7 @@
+package com.wsr.user
+
+interface UserRepository {
+    suspend fun getUser(id: UserId): User
+    suspend fun getUsers(ids: List<UserId>): List<User>
+    suspend fun createUser(user: User)
+}
