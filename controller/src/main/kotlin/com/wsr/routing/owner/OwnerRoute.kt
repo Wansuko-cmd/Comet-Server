bcb369a4855163ccadec837ba7c81ac17fe89441
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2024-11-26 07:41:07 +0900

    [add] OwnerUsersIndexGetを追加

--- controller/src/main/kotlin/com/wsr/routing/owner/OwnerRoute.kt
+++ controller/src/main/kotlin/com/wsr/routing/owner/OwnerRoute.kt
@@ -1,11 +1,13 @@
 package com.wsr.routing.owner
 
 import com.wsr.routing.owner.comets.ownerCometsIndexGet
+import com.wsr.routing.owner.user.ownerUsersIndexGet
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.route
 
 fun Route.ownerRoute() {
     route("/owner") {
         ownerCometsIndexGet()
+        ownerUsersIndexGet()
     }
 }
---
+++ controller/src/main/kotlin/com/wsr/routing/owner/user/OwnerUsersIndexGet.kt
@@ -0,0 +1,56 @@
+package com.wsr.routing.owner.user
+
+import com.wsr.mapBoth
+import com.wsr.routing.getRequest
+import com.wsr.user.GetUserUseCase
+import com.wsr.user.User
+import com.wsr.user.UserId
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
+import io.ktor.util.reflect.TypeInfo
+import kotlinx.serialization.Serializable
+import org.koin.ktor.ext.inject
+
+fun Route.ownerUsersIndexGet() {
+    val getUserUseCase: GetUserUseCase by inject()
+
+    get("") {
+        call
+            .getRequest<OwnerUsersIndexGetRequest>()
+            .let { (ownerId) -> getUserUseCase(UserId.OwnerId(ownerId)) }
+            .mapBoth(
+                success = { user ->
+                    call.respond(
+                        message = OwnerUsersIndexGetResponse.from(user),
+                        typeInfo = TypeInfo(Unit::class),
+                    )
+                },
+                failure = {
+                    call.respond(
+                        message = HttpStatusCode.BadRequest,
+                        typeInfo = TypeInfo(Unit::class),
+                    )
+                },
+            )
+    }
+}
+
+@Serializable
+private data class OwnerUsersIndexGetRequest(
+    val ownerId: String,
+)
+
+@Serializable
+private data class OwnerUsersIndexGetResponse(
+    val id: String,
+    val name: String,
+) {
+    companion object {
+        fun from(user: User) =
+            OwnerUsersIndexGetResponse(
+                id = user.id.value,
+                name = user.name.value,
+            )
+    }
+}
