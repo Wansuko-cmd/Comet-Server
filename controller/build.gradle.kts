3ea157c38099f01ff96ad7e14650f6a23f89f979
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2024-11-26 00:09:17 +0900

    [add] 依存関係を追加

--- controller/build.gradle.kts
+++ controller/build.gradle.kts
@@ -1,8 +1,19 @@
 plugins {
     kotlin("jvm")
+    alias(libs.plugins.serialization)
+    alias(libs.plugins.ktor)
 }
 
 dependencies {
+    implementation(project(":domain"))
+    implementation(project(":usecase"))
+    implementation(project(":utils"))
+
     implementation(libs.kotlin.coroutine)
     implementation(libs.kotlin.datetime)
+
+    implementation(libs.kotlin.serialization)
+    implementation(libs.bundles.ktor)
+    implementation(platform(libs.koin.bom))
+    implementation(libs.bundles.koin)
 }
