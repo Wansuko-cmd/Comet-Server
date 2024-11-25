5a5ced331031c1129f798a2a42234297ee459aae
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2024-11-26 07:10:22 +0900

    [add] OwnerRouteを追加

--- controller/src/main/kotlin/com/wsr/routing/MainRoute.kt
+++ controller/src/main/kotlin/com/wsr/routing/MainRoute.kt
@@ -1,6 +1,7 @@
 package com.wsr.routing
 
 import com.wsr.routing.comets.cometsRoute
+import com.wsr.routing.owner.ownerRoute
 import com.wsr.routing.users.usersRoute
 import io.ktor.server.application.Application
 import io.ktor.server.routing.get
@@ -9,6 +10,7 @@ import io.ktor.server.routing.routing
 fun Application.mainRoute() {
     routing {
         get("") { call.respond("Success", typeInfo = null) }
+        ownerRoute()
         cometsRoute()
         usersRoute()
     }
