8edac7f31a87389f5605c30da1048e438a8daa3d
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2024-11-26 00:12:04 +0900

    [add] main関数周りを追加

---
+++ controller/src/main/kotlin/com/wsr/Application.kt
@@ -0,0 +1,11 @@
+package com.wsr
+
+import io.ktor.server.application.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>) {
+    EngineMain.main(args)
+}
+
+fun Application.module() {
+}
