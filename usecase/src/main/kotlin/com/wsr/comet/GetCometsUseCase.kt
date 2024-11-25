e7636a5439f634cdbadc629d70abc8025360b093
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2024-11-25 23:51:27 +0900

    [fix] Get系の返り値を定義

--- usecase/src/main/kotlin/com/wsr/comet/GetCometsUseCase.kt
+++ usecase/src/main/kotlin/com/wsr/comet/GetCometsUseCase.kt
@@ -1,21 +1,42 @@
 package com.wsr.comet
 
+import com.wsr.user.User
 import com.wsr.user.UserId
+import com.wsr.user.UserRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class GetCometsUseCase(
-    private val repository: CometRepository,
+    private val cometRepository: CometRepository,
+    private val userRepository: UserRepository,
     private val dispatcher: CoroutineDispatcher = Dispatchers.Default,
 ) {
     suspend operator fun invoke(
         ownerId: UserId.OwnerId,
         page: Int,
-    ): List<Comet> = withContext(dispatcher) {
-        repository.getComets(
+    ): List<GetComet> = withContext(dispatcher) {
+        val comets = cometRepository.getComets(
             ownerId = ownerId,
             offset = page,
         )
+        val users = userRepository.getUsers(ids = comets.map { it.ownerId })
+        comets.map { comet ->
+            GetComet(
+                id = comet.id,
+                ownerUser = users.first { it.id == comet.ownerId },
+                core = comet.core,
+                coma = comet.coma,
+                tails = comet.tails,
+            )
+        }
     }
 }
+
+data class GetComet(
+    val id: CometId,
+    val ownerUser: User,
+    val core: Core,
+    val coma: Coma?,
+    val tails: List<Tail>,
+)
--- usecase/src/main/kotlin/com/wsr/comet/GetOwnedCometsUseCase.kt
+++ usecase/src/main/kotlin/com/wsr/comet/GetOwnedCometsUseCase.kt
@@ -1,21 +1,46 @@
 package com.wsr.comet
 
+import com.wsr.user.User
 import com.wsr.user.UserId
+import com.wsr.user.UserRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 
 class GetOwnedCometsUseCase(
-    private val repository: CometRepository,
+    private val cometRepository: CometRepository,
+    private val userRepository: UserRepository,
     private val dispatcher: CoroutineDispatcher = Dispatchers.Default,
 ) {
     suspend operator fun invoke(
         ownerId: UserId.OwnerId,
         page: Int = 0,
-    ): List<Comet.Owned> = withContext(dispatcher) {
-        repository.getOwnedComets(
-            ownerId = ownerId,
-            offset = page,
-        )
+    ): List<GetOwnedComet> = withContext(dispatcher) {
+        val ownedComets = async {
+            cometRepository.getOwnedComets(
+                ownerId = ownerId,
+                offset = page,
+            )
+        }
+        val ownedUser = async { userRepository.getUser(ownerId) }.await()
+        ownedComets.await().map { ownedComet ->
+            GetOwnedComet(
+                id = ownedComet.id,
+                ownerUser = ownedUser,
+                core = ownedComet.core,
+                coma = ownedComet.coma,
+                tails = ownedComet.tails,
+            )
+        }
     }
 }
+
+data class GetOwnedComet(
+    val id: CometId,
+    val ownerUser: User,
+    val core: Core,
+    val coma: Coma?,
+    val tails: List<Tail>,
+)
+
